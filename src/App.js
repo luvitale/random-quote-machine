1b1e7fbe7424808539367438b72daf544c5ecc09
Luciano Nahuel Vitale <lunavi97@gmail.com>
2021-01-26 03:05:41 -0300

    Added new quote button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App" id="quote-box">
 		<p id="text">Text</p>
 		<p id="author">Author</p>
+		<button type="button" id="new-quote">New quote</button>
     </div>
   );
 };
